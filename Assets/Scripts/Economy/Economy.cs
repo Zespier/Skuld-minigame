d32b6a208aa4299f5d6dd268b55cb126b8ec1ac6
Zespier <pabloreco.redondo@gmail.com>
2025-01-25 09:17:58 +0100

    Economy letters

--- Assets/Scripts/Economy/Economy.cs
+++ Assets/Scripts/Economy/Economy.cs
@@ -10,6 +10,7 @@ public class Economy : MonoBehaviour {
     public TMP_Text soulsPerSecond;
     public TMP_Text totalSouls;
     public List<EconomyUpgrade> upgrades = new List<EconomyUpgrade>();
+    public List<char> economyLetters = new List<char>() { ' ', 'K', 'M', 'B', 'T', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' };
 
     private void Start() {
         stats.ManageAddUpgrade(Buff.BaseSoulsPerSecond, 6);
@@ -18,11 +19,19 @@ public class Economy : MonoBehaviour {
 
     private void Update() {
         souls += stats.SoulsPerSecond * Time.deltaTime;
-        soulsPerSecond.text = $"{stats.SoulsPerSecond:F0} Soul/s";
-        totalSouls.text = $"{souls:F0} S";
+        soulsPerSecond.text = $"{TransformIntoEconomyLetter(stats.SoulsPerSecond)} Soul/s";
+        totalSouls.text = $"{TransformIntoEconomyLetter(souls)}";
 
         if (Input.GetKeyDown(KeyCode.U)) {
             stats.ManageAddUpgrade(upgrades[0].buffType, upgrades[0].amount);
         }
+    }
+
+    private string TransformIntoEconomyLetter(float amount) {
+        int ceroAmount = ((int)amount).ToString().Length - 1; //Quitando la unidad
+
+        int stepsIntoTheEconomyLetters = (ceroAmount / 3);
+
+        return (amount / Mathf.Pow(10, stepsIntoTheEconomyLetters * 3)).ToString("F2") + economyLetters[stepsIntoTheEconomyLetters];
     }
 }
