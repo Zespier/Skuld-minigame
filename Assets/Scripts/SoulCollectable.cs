704fac26e45a62f4cf592de1c1eeab96e2b5d7f8
Zespier <pabloreco.redondo@gmail.com>
2025-01-25 13:52:22 +0100

    Animation of the soul

--- Assets/Scripts/SoulCollectable.cs
+++ Assets/Scripts/SoulCollectable.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SoulCollectable : MonoBehaviour {
+
+    public Animator animator;
+
+    private async void Start() {
+        await Task.Delay(Random.Range(0, 500));
+        animator.Play("Idle");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
