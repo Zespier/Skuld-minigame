4fd53906726b362d18d360f84d7e8bebc9010fa2
Zespier <pabloreco.redondo@gmail.com>
2025-01-25 05:58:56 +0100

    Existen mejroas con sus nombres y descriptciones, y se añaden correctamente al jueguito

--- Assets/Scripts/Economy/Economy.cs
+++ Assets/Scripts/Economy/Economy.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Economy : MonoBehaviour {
 
     public Stats stats;
     public float souls;
+    public TMP_Text soulsPerSecond;
+    public TMP_Text totalSouls;
+    public List<EconomyUpgrade> upgrades = new List<EconomyUpgrade>();
 
-    private void Awake() {
+    private void Start() {
         stats.ManageAddUpgrade(Buff.BaseSoulsPerSecond, 6);
         stats.ManageAddUpgrade(Buff.SoulsPerSecondPercentage, 50);
     }
 
     private void Update() {
         souls += stats.SoulsPerSecond * Time.deltaTime;
+        soulsPerSecond.text = $"{stats.SoulsPerSecond:F0} Soul/s";
+        totalSouls.text = $"{souls:F0} S";
+
+        if (Input.GetKeyDown(KeyCode.U)) {
+            stats.ManageAddUpgrade(upgrades[0].buffType, upgrades[0].amount);
+        }
     }
 }
---
+++ Assets/Scripts/Economy/EconomyUpgrade.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EconomyUpgrade", menuName = "EconomyUpgrade")]
+public class EconomyUpgrade : ScriptableObject {
+
+    public Buff buffType;
+    public float amount;
+    public string Name;
+    public string description;
+
+}
